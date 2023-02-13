cec4b198b3f640d4a13b5be81293049ba758b07d
Scott Tippetts <wstippetts+github@gmail.com>
2023-02-13 15:49:04 -0700

    fake db built

---
+++ server/db/Fakedb.js
@@ -0,0 +1,15 @@
+class Fakedb {
+
+  burgers = [{
+    name: 'big blue',
+    toppings: 'little bit of everything'
+  }, {
+    name: 'cowboy',
+    toppings: 'not quite as much of everything'
+
+  }]
+
+}
+
+
+export const fakedb = new Fakedb()
